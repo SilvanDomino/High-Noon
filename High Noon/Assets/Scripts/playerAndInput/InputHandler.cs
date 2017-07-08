70ab728863ba4bbb6d5eab0072152b65133b9159
Erwin Henraat <e.henraat@ma-web.nl>
2017-07-08 13:07:10 +0200

    horizontal movement done, jump work in progress

---
+++ High Noon/Assets/Scripts/playerAndInput/InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// InputHandler. class handles input connects the input to the players and calls player methods. 
+/// </summary>
+public class InputHandler : MonoBehaviour {
+	[SerializeField]private byte playerNumber = 1;
+	private MoveHorizontal mhScript;
+	private Jump jScript;
+	// Use this for initialization
+	void Start () {
+		mhScript = gameObject.GetComponent<MoveHorizontal>();
+		jScript = gameObject.GetComponent<Jump>();	
+	}	
+	// Update is called once per frame
+	void Update () {
+			
+		float hValue = Input.GetAxis("Horizontal"+playerNumber);
+		float vValue = Input.GetAxis("Vertical"+playerNumber);
+		//print(playerNumber + " : h : "+hValue+ " : v : "+ vValue);
+
+		//Handle player methods
+		mhScript.Input = hValue;
+		jScript.Input = vValue;
+	}
+}
---
+++ High Noon/Assets/Scripts/playerAndInput/Jump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+	private float inputValue = 0;
+	private Rigidbody2D rb;
+	private float lastInputValue = 0;
+	[SerializeField]private float maxJumpForce = 5;
+	[SerializeField]private byte maxJumps = 2;
+	private float currentJumpForce = 0;
+	private byte numJumps = 0;
+
+
+	public float Input
+	{
+		set{ inputValue = value; }
+	}
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}	
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		
+		//jumpbutton released
+		if(inputValue < lastInputValue){
+
+			if(numJumps < maxJumps){
+				rb.AddForce(Vector2.up * maxJumpForce * inputValue);
+				numJumps++; 
+			}
+		}
+
+
+
+
+		lastInputValue = inputValue;
+	}
+}
