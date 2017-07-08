f3a1715cdc1093775ab53756fe553d402ec0adf4
Silvan Herrema <silvah489@gmail.com>
2017-07-08 12:16:14 +0200

    PlatformHealth

--- High Noon/Assets/Scripts/Platform/PlatformCollision.cs
+++ High Noon/Assets/Scripts/Platform/PlatformCollision.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformCollision : MonoBehaviour {
+namespace Platform
+{
+    public class PlatformCollision : MonoBehaviour
+    {
+        
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
+
---
+++ High Noon/Assets/Scripts/Platform/PlatformHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platform
+{
+    public class PlatformHealth : MonoBehaviour
+    {
+        [SerializeField]
+        private int _health = 5;
+
+        public int Health
+        {
+            get { return _health; }
+        }
+
+        public void DamageHealth(int damage)
+        {
+            this._health -= damage;
+            if(this._health <= 0)
+            {
+                //destroy tile
+            }
+        }
+    }
+}
+
