43628dc9d666caa88a50277e7aa30f0ec9fdc388
Richard Ram <richard@richard-ram.com>
2017-07-08 19:25:05 +0200

    Implemented gameflow

--- High Noon/Assets/Scripts/GameManager.cs
+++ High Noon/Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour 
+public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _winScreenObj;
+    [SerializeField] private WinScreenUI _winScreen;
+    
     public enum GameState
     {
         Countdown,
-        Gameplay
+        Gameplay,
+        WinScreen
     }
 
     private GameState _currentState;
@@ -18,9 +23,11 @@ public class GameManager : MonoBehaviour
     }
 
     public static GameManager Instance;
+
+    public WinData WinData{ get; private set; }
     
     private void Awake()
-    {
+    {     
         if (Instance != null)
         {
             Destroy(this);
@@ -32,8 +39,38 @@ public class GameManager : MonoBehaviour
         CountdownSystem.Countdown.OnCountdownDone += AfterCountdown;
     }
 
+    private void Start()
+    {
+        _winScreenObj.SetActive(false);
+    }
+   
     private void AfterCountdown()
     {
         _currentState = GameState.Gameplay;
+    }
+
+    public void GameWon(Player p)
+    {
+        //print(winningPlayer.gameObject.name + " WON");
+        _currentState = GameState.WinScreen;
+        _winScreen.UpdateUI(new WinData(p.name, p.ID));
+        _winScreenObj.SetActive(true);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
+public struct WinData
+{
+    public string name;
+    public int nr;
+
+    public WinData(string name, int nr)
+    {
+        this.name = name;
+        this.nr = nr;
     }
 }
---
+++ High Noon/Assets/Scripts/WinScreenUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreenUI : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    
+    public void UpdateUI(WinData data)
+    {
+        _text.text = data.name + " Won!";
+    }
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+           GameManager.Instance.ReloadScene();
+    }
+}
