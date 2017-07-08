a6546754569babd4d81270f08a5aa0abe8e06cd1
Silvan Herrema <silvah489@gmail.com>
2017-07-08 19:34:14 +0200

    fixed layers, going to add collidable to mid camera

---
+++ High Noon/Assets/Scripts/Platform/MultipleLayers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleLayers : MonoBehaviour {
+
+    void Awake()
+    {
+        var layer2 = LayerMask.NameToLayer("Platform");
+        var layer1 = LayerMask.NameToLayer("Collidable");
+
+        int layermask1 = 1 << layer1;
+        int layermask2 = 1 << layer2;
+
+        int finalmask = layermask1 | layermask2;
+    }
+}
