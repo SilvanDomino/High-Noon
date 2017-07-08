cf9a644dce0c9f274579bc12b1850acd78658b3c
Silvan Herrema <silvah489@gmail.com>
2017-07-08 18:57:22 +0200

    worst camerashake ever

---
+++ High Noon/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    private Vector3 magnitude = new Vector3(0, 0.03f, 0);
+    private float update = 0.004f;
+
+	public void Shake()
+    {
+        StartCoroutine("Shaker");
+    }
+
+    IEnumerator Shaker()
+    {
+        gameObject.transform.position += magnitude;
+        yield return new WaitForSeconds(update);
+        gameObject.transform.position -= magnitude;
+    }
+}
