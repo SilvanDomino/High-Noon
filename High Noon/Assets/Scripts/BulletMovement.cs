6a6f81184a42c99ebe66f05dac8b404dd7a5b221
Richard Ram <richard@richard-ram.com>
2017-07-08 14:56:27 +0200

    Implemented a basic bulletsystem and tagmanager

---
+++ High Noon/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BulletSystem
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BulletMovement : MonoBehaviour
+    {
+        [SerializeField] private float _initialsSpeed;
+        private float _currentSpeed;
+
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            _currentSpeed = _initialsSpeed;
+        }
+        
+        private void FixedUpdate()
+        {
+            var velocity = new Vector2(transform.up.x, transform.up.y) * _currentSpeed * Time.deltaTime;
+            _rigidbody.MovePosition(_rigidbody.position + velocity);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            
+            if (!other.gameObject.CompareTag(TagManager.COLLIDER)) return; 
+                
+            var collidable = other.gameObject.GetComponent<ICollidable>();
+            if (collidable != null)
+                collidable.Collide();
+               
+            BounceBullet(other);
+        }
+
+        private void BounceBullet(Collision2D other)
+        {
+            print(other.contacts.Length);
+            var newDirection = Vector2.Reflect(transform.up, other.contacts[0].normal);
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, newDirection);
+        }
+    }
+}
---
+++ High Noon/Assets/Scripts/TagManager.cs
@@ -0,0 +1,5 @@
+﻿
+public class TagManager
+{
+    public const string COLLIDER = "Collidable";
+}
